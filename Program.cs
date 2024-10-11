faca38d6046d15c200e192a75c21422cab68d002
BenceR27 <rizb@kkszki.hu>
2024-10-11 12:21:43 +0200

    The program is ready to use

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kamat
+{
+    internal class Program
+    {
+        static void Kamat()
+        {
+            Console.WriteLine("Mennyi pénzt szeretnél befektetni? ");
+            var osszeg = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hány évre szeretnéd ezt az összeget befektetni? ");
+            int ev = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mennyi a kamat? ");
+            double kamat = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mennyi a kamatozási periódusok száma évente? ");
+
+
+            double vegosszeg = osszeg * Math.Pow(1 + kamat / 100, ev);
+
+            Console.WriteLine($"{ev} év múlva: {vegosszeg:F2}Ft");
+        }
+
+        static void Main(string[] args)
+        {
+            Kamat();
+        }
+    }
+}
